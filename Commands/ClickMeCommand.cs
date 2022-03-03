702e8f8fd61e00fa0d23c24346055631fcd3b710
Maroua Khadri <mkha@cairful.com>
2022-03-03 17:27:39 +0100

    Add generic delegate

---
+++ Commands/ClickMeCommand.cs
@@ -0,0 +1,36 @@
+﻿using ReserveRoom.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReserveRoom.Commands
+{
+    public class ClickMeCommand : CommandBase
+    {
+
+        public delegate T MyAction<T>(T m);
+
+        public MyAction<DateTime> MyStrAction { get; }
+
+         public ClickMeCommand(MyAction<DateTime> myAction)
+         {
+            MyStrAction = myAction;
+         }
+
+
+        public override bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public int Counter = 0;
+        public override void Execute(object parameter)
+        {
+            MyStrAction(DateTime.Now);
+        }
+       
+
+
+    }
+}
